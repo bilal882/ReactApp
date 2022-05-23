e09a78ea0cda9dcabebede41c7d43ea2a6d5f103
Muhammad Bilal Iqbal <98903105+bilal882s@users.noreply.github.com>
2022-05-22 20:51:56 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>It is my first web of codesandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button>Click Me</button>
     </div>
   );
 }
